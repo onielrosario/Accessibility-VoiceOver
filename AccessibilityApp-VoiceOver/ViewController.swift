0bd80463172759531e51587efb082a987e8ee238
Oniel Rosario <orose2689@gmail.com>
2020-07-23 10:48:24 -0600

    Initial Commit

---
+++ AccessibilityApp-VoiceOver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessibilityApp-VoiceOver
+//
+//  Created by Oniel Rosario on 7/23/20.
+//  Copyright © 2020 Oniel Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
